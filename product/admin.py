63316cdccf679151886bddcc7729fc76229791f5
Nurbek <nurbekkalilov@gmail.com>
2021-07-07 14:23:22 +0600

    Home work for lesson 1

---
+++ product/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from product.models import *
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+    search_fields = ['title',  'description',]
+    list_display = 'id title category price'.split()
+    list_filter = 'category'.split()
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Review)
