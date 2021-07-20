e26cf0f3bf17b57fc2adaff4aa009bb0b510807b
Nurbek <nurbekkalilov@gmail.com>
2021-07-20 11:02:38 +0600

    Home work

--- Django_Project_HW/urls.py
+++ Django_Project_HW/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from product import views
@@ -22,4 +24,4 @@ urlpatterns = [
     path('hello/', views.hello_world_view),
     path('', views.main_page_view),
     path('product/<int:product_id>/', views.product_item_view)
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
